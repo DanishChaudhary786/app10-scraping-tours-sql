e7e3df7053617370d528a067d9220dfb942eddd6
DC <chaudharydanish024@gmail.com>
2024-07-29 11:46:35 +0530

    Using Class & Method

--- main.py
+++ main.py
@@ -12,58 +12,62 @@ HEADERS = {
 connection = sqlite3.connect("app10-scraping-tours-sql.db")
 
 
-def scrape(url):
-    """Scrap the page scource from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Event:
+    def scrape(self, url):
+        """Scrap the page scource from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+        sender_email = "danish.twilio@gmail.com"
+        reciver_email = "chaudharydanish024@gmail.com"
+        sender_pass = "afmbdbtjakbmymeb"
+        # message = "Subject: Next Upcoming Tour reminder \n\n" + message
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(sender_email, sender_pass)
+            server.sendmail(sender_email, reciver_email, message)
+        print("Email Sent!!! ")
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-    sender_email = "danish.twilio@gmail.com"
-    reciver_email = "chaudharydanish024@gmail.com"
-    sender_pass = "afmbdbtjakbmymeb"
-    # message = "Subject: Next Upcoming Tour reminder \n\n" + message
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(sender_email, sender_pass)
-        server.sendmail(sender_email, reciver_email, message)
-    print("Email Sent!!! ")
+class Database:
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = connection.cursor()
+        cursor.execute("insert into events values(?,?,?)", row)
+        connection.commit()
 
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("insert into events values(?,?,?)", row)
-    connection.commit()
-
-
-def read_data(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date =row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def read_data(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        extracted = extract(scrape(URL))
+        event = Event()
+        extracted = event.extract(event.scrape(URL))
         print(extracted)
         if extracted.lower() != "no upcoming tours":
-            if not read_data(extracted):
-                store(extracted)
-                # send_email(message="Hey, new event was found")
+            db = Database()
+            if not db.read_data(extracted):
+                db.store(extracted)
+                email = Email()
+                # email.send(message="Hey, new event was found")
         time.sleep(3)
